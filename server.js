2044089332a1a9b26410f7a47e92816084c57916
Terry <32140723+trrychng@users.noreply.github.com>
2018-03-03 08:23:55 -0500

    Update server.js

--- server.js
+++ server.js
@@ -149,7 +149,7 @@ passport.use(new LocalStrategy(
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync({
-  force: true
+  force: false
 }).then(function () {
   app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
